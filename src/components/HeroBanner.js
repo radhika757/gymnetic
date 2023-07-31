3888984abe715d273d7aad349c6522beb1b0d272
Radhika Rao <raoradhika2000@gmail.com>
2023-07-31 23:41:16 +0530

    search bar design ready

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -16,15 +16,31 @@ const HeroBanner = () => {
       <Typography
         fontWeight="700"
         sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
       >
         Sweat,Smile <br /> and Repeat
       </Typography>
-      <Typography fontSize="22px" lineHeight="35px" mb={2}>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exercises.
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
         Explore Exercises
       </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+        fontSize="200px"
+        mt={4}
+      >
+        Exercise
+      </Typography>
       <img src={banner} alt="main-img" className="hero-banner-img" />
     </Box>
   );
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Button, TextField, Stack, Typography } from "@mui/material";
+// TextField is an input
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  return (
+    <Stack alignItems="center" mt="-20px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Core Exercises you <br />
+        should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "4px",
+            },
+            width: {
+              lg: "800px",
+              xs: "350px",
+            },
+            backgroundColor: "#ffffff",
+            borderRadius: "40px",
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search for a workout"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#ffffff",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
