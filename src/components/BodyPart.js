69ab7b75079f9560378bc25d86209b230f1ee911
Radhika Rao <raoradhika2000@gmail.com>
2023-08-07 23:33:58 +0530

    Horizontal scrollbar ready

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -18,8 +18,20 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
     >
-      <img src={Icon} alt="dumbel" styles={{ width: "40px", height: "40px" }} />
+      <img src={Icon} alt="dumbel" style={{ width: "40px", height: "40px" }} />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Exercises = () => {
-  return <div>Exercises</div>;
+  return (
+    <>
+      <div>Exercises</div>
+      <h2>Exercises</h2>
+    </>
+  );
 };
 
 export default Exercises;
